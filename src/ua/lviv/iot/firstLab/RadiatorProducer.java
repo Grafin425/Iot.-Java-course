a731631f3b84c4cba3a574afc31a5b6ca9828445
Yaroslav Yedyn <clavik.edin@gmail.com>
2020-02-12 23:45:05 +0200

    fixed the constructor error

--- src/ua/lviv/iot/firstLab/RadiatorProducer.java
+++ src/ua/lviv/iot/firstLab/RadiatorProducer.java
@@ -3,9 +3,9 @@ package ua.lviv.iot.firstLab;
 public class RadiatorProducer {
     public static void main(String[] args) {
         Radiator.numberOfParts=3;
-       Radiator firstRadiator = new Radiator(200, "black", "USA", 1500, 50000, "outdoor");
+       Radiator firstRadiator = new Radiator();
         Radiator secondRadiator = new Radiator(150, "White", "UA", 1500, 35000, "outdoor");
-        Radiator thirdRadiator = new Radiator(150, "black", "UA", 2000, 55000, "built in");
+        Radiator thirdRadiator = new Radiator(150, "black", "UA", 2000, 55000);
         System.out.println(firstRadiator);
         System.out.println(secondRadiator);
         System.out.println(thirdRadiator);
